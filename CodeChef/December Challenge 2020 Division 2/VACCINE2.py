f9fe2ffbe7b2a8a1149a60b6b3cf4d8fb6bc7480
lucky verma <thinkersloop@gmail.com>
2020-12-13 20:10:42 +0530

    .gitignore

---
+++ CodeChef/December Challenge 2020 Division 2/VACCINE2.py
@@ -0,0 +1,17 @@
+for _ in range(int(input())):  # takes the input cases
+    N, D = map(int, input().split())
+    agesList = list(map(int, input().split()))
+    risky, normal = 0, 0
+    for k in agesList:
+        if k <= 9 or k >= 80:
+            risky = risky + 1
+        else:
+            normal = normal + 1
+    Rday = risky // D
+    Nday = normal // D
+    if risky % D != 0:
+        Rday = Rday + 1
+    if normal % D != 0:
+        Rday = Rday + 1
+    Rday = Rday + Nday
+    print(Rday)
--- DS & Algo/Coursera/ALGO TOOLBOX/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
+++ DS & Algo/Coursera/ALGO TOOLBOX/week2_algorithmic_warmup/1_fibonacci_number/fibonacci.py
@@ -1,9 +1,16 @@
-# Uses python3
-def calc_fib(n):
-    if (n <= 1):
-        return n
+# # Uses python3
+# def calc_fib(n):
+#     if (n <= 1):
+#         return n
 
-    return calc_fib(n - 1) + calc_fib(n - 2)
+#     return calc_fib(n - 1) + calc_fib(n - 2)
+
+# n = int(input())
+# print(calc_fib(n))
+
 
 n = int(input())
-print(calc_fib(n))
+F = [0, 1]
+for i in range(2, n + 1):
+    F.append(F[i - 1] + F[i - 2])
+print(F[n])
