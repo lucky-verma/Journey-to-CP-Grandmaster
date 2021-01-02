3159241fc0b9ac28e035ef47a138194c2b169d03
lucky verma <thinkersloop@gmail.com>
2021-01-02 16:33:21 +0530

    Jan Long

---
+++ CodeChef/January Challenge 2021 Division 3/DECODEIT.py
@@ -0,0 +1,82 @@
+"""
+Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
+"""
+# ------------------- fast io --------------------
+import os
+import sys
+from io import BytesIO, IOBase
+
+BUFSIZE = 8192
+
+
+class FastIO(IOBase):
+    newlines = 0
+
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+
+
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+
+
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+
+# ------------------- fast io --------------------
+if __name__ == "__main__":
+    for _ in range(int(input())):
+        N = int(input())
+        B = list(input())
+        b = []
+        slc = N // 4
+        for i in range(slc):
+                b.append(B[:4])
+                del B[:4]
+        ans = []
+        for k in b:
+            AlpLst = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p']
+            for i in range(len(k)):
+                if k[i] == '0':
+                    AlpLst = AlpLst[:len(AlpLst)//2]
+                else:
+                    AlpLst = AlpLst[len(AlpLst)//2:]
+            ans.append(*AlpLst)
+        yo = ''.join(map(str, ans))
+        print(yo)
+
+
+
