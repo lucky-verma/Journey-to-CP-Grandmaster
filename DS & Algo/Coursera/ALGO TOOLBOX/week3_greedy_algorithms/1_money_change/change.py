05cbd26fb90b3290cc8404c741ef96546c180c28
lucky verma <thinkersloop@gmail.com>
2021-01-03 19:28:43 +0530

    week3 geeedy algo

--- DS & Algo/Coursera/ALGO TOOLBOX/week3_greedy_algorithms/1_money_change/change.py
+++ DS & Algo/Coursera/ALGO TOOLBOX/week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,18 @@
 # Uses python3
 import sys
 
+
 def get_change(m):
-    #write your code here
-    return m
+    # write your code here
+    changes = [10, 5, 1]
+    c = 0
+    for i in range(len(changes)):
+        n = int(m / changes[i])
+        c += n
+        m -= n * changes[i]
+    return c
+
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
--- DS & Algo/Coursera/ALGO TOOLBOX/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ DS & Algo/Coursera/ALGO TOOLBOX/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -1,17 +1,32 @@
 # Uses python3
 import sys
 
+
 def get_optimal_value(capacity, weights, values):
     value = 0.
     # write your code here
-
+    value = 0
+    for i in range(n):
+        if capacity == 0:
+            return value
+        a = min(capacity, weights[values[i]])
+        value = value + values[i] * a
+        capacity = capacity - a
     return value
 
 
 if __name__ == "__main__":
-    data = list(map(int, sys.stdin.read().split()))
-    n, capacity = data[0:2]
-    values = data[2:(2 * n + 2):2]
-    weights = data[3:(2 * n + 2):2]
+    c = input()
+    n, capacity = map(int, c.split())
+    values = []
+    weights = {}
+
+    for i in range(n):
+        c = input()
+        v, w = map(int, c.split())
+        values.append(v / w)
+        weights[v / w] = w
+    values.sort(reverse=True)
+
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
